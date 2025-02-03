e0a53568ea751f75d5dee1235f594b465de06f48
barcek2281 <sabdpp17@gmail.com>
2025-02-03 23:26:21 +0500

    new bug found

--- middleware/middleware.js
+++ middleware/middleware.js
@@ -2,6 +2,7 @@ async function authenticateUser(req, res, next) {
     if (!req.session.user || !req.session.user._id) {
         return res.redirect("/login"); // Если нет сессии, отправляем на логин
     }
+    next()
 }
 
 module.exports = authenticateUser;
